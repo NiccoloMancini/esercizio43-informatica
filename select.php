22ab640605ac39cc36cf159b8ef0367b79afeee7
Niccolo <niccolomancini3@gmail.com>
2025-02-13 08:36:38 +0100

    fine esercizio 43

---
+++ select.php
@@ -0,0 +1,39 @@
+<?php
+    include("connessione.php");
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <table class="table text-center">
+    <tr>
+    <?php
+      if($result = $conn->query("SELECT * FROM ATTORI WHERE AnnoNascita BETWEEN '".$_GET["annominimo"]."' AND '".$_GET["annomassimo"] . "' ORDER BY ".$_GET["campo"]." ASC")){
+        foreach($result->fetch_fields() as $columnName){
+          echo "<th>". $columnName->name. "</th>";
+        }
+        echo "</tr>";
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            foreach($row as $column){
+              echo "<td>". $column. "</td>";
+            }
+            echo "</tr>";
+          }
+        }else{
+          echo "<h1 class='text-center'>Nessun attore trovato con anno di nascita compreso tra " . $_GET["annominimo"] . " e " . $_GET["annomassimo"] . "</h1>";
+        }
+      }
+    ?>
+    </table>
+    <a href="./attorieta.html">index</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
